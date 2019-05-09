758b7054352dab57f0b167bf1000ea745f8b6b8f
TechGeekD <TechGeekD@gmail.com>
2019-05-09 18:18:33 +0530

    Updated: environment configuration to use kReleaseMode

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
+
 import 'package:flutter_todo_redux/env/configs/dev.dart' as dev;
 import 'package:flutter_todo_redux/env/configs/prod.dart' as prod;
 
-bool prodEnv = false;
+bool prodEnv = kReleaseMode;
 
 Map<String, dynamic> get env => prodEnv ? prod.config : dev.config;
